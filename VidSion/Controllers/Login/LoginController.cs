55d401ce1603e9e1ed3710490cd7e1877327797a
Benny Ooi <bennykeanhoe.ooi@sjsu.edu>
2020-10-06 01:19:31 -0700

    First Commit

---
+++ VidSion/Controllers/Login/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VidSion.Controllers.Login
+{
+    public class LoginController : Controller
+    {
+        // GET: LoginController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
